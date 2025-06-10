4c7b1c8aa7f4b9400f1cdb1f2fb180363a15927d
vantu03 <concaoheo203@gmail.com>
2025-06-11 03:27:50 +0700

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="autowriter-vt",
-    version="0.1.0",
+    version="0.1.1",
     author="Ma Văn Tú",
     description="AI-based content generator for Vietnamese articles",
     long_description=open("README.md", encoding="utf-8").read(),
